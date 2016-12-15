629cd1bdaadf5f14d257a32f5e5f3f54dbca3e80
lias <lias@s-virtualmachine10-lias.w.ensma.fr>
2016-12-15 20:29:17 +0100

    Change path for postgres

--- 02_configureallnodes.sh
+++ 02_configureallnodes.sh
@@ -5,8 +5,9 @@ tar xfv download
 cd /home/postgres/postgres-xl-9.5r1.4/ 
 ./configure
 make
+make install
 ufw disable
 echo "PATH='/usr/local/pgsql/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'" | sudo tee /etc/environment
 mkdir -p /home/postgres/.ssh
-ssh-keygen -f .ssh/id_rsa -t rsa -N ''
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -f /home/postgres/.ssh/id_rsa -t rsa -N ''
+cat /home/postgres/.ssh/id_rsa.pub | tee -a /home/postgres/.ssh/authorized_keys
